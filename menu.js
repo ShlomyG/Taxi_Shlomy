5498a35f4272ca7df7f3fdc5236b3d36ecd79a2b
ShlomyG <Shlomyg7@gmail.com>
2020-05-19 12:51:40 +0300

    only-index

---
+++ menu.js
@@ -0,0 +1,12 @@
+var menuLinks = [
+    ' <a href="index.html">Home</a> | ',
+    ' <a href="drivers.html">Drivers</a> | ',
+    ' <a href="services.html">Services</a> | ',
+    '<a href="callacab.html">Call a cab</a> | ',
+     ' <a href="contact-us.html">Contact Us</a> | '
+  ]
+  
+  for (var i = 0 ; i < menuLinks.length ; i++) {
+      document.write(menuLinks[i])
+  }
+  
